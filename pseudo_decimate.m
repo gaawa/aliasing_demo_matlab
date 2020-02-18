e48c241341517377a25d0e43c6078a5388971348
HagimuraN <17564966+gaawa@users.noreply.github.com>
2020-02-18 15:58:21 +0100

    removed f_eff printing to console

--- pseudo_decimate.m
+++ pseudo_decimate.m
@@ -1,6 +1,6 @@
 function [y, fs_eff]=pseudo_decimate(Nsamp, fs, dec_n, y, mode)
 dec_factor=2^dec_n;
-fs_eff = fs/dec_factor; % print effective sampling frequency
+fs_eff = fs/dec_factor;
 if dec_n~=0
     % Decimate signal
     switch mode
